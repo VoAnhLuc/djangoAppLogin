c75c1142bafb18000d1ed715f0588f095460cfcf
Dev <developer@goldfishcode.com>
2023-04-11 10:10:57 +0700

    fix 10:09 11/4

--- login/urls.py
+++ login/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register, name='register'),
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
 
 ]
